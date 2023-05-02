a0c6a90f19ecdaaa358c3b3cba4f9bfded7f9819
Olivier Otschudi <olivier.otschudi@gmail.com>
2023-05-02 10:37:16 +0200

    on continue lentement mais surement

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+// Sélectionner les éléments 
+const score0El = document.getElementById('score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+// condition de départ
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
